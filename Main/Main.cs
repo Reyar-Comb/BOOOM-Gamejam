91f7ccd96579eb9b41d190f57b68d69ba181cef0
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2026-04-29 23:47:43 +0800

    chore: update Main.tscn for testing

---
+++ Main/Main.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+public partial class Main : Node2D
+{
+	[Export] public VarManager VarManager { get; set; }
+	private Var _var;
+	public override void _Ready()
+	{
+		Var var = new Var();
+		var.Stats = new VarStats
+		{
+			MaxHealth = 100,
+			AttackSpeed = 1.5f,
+			MoveSpeed = 100f,
+			Position = new Vector2(0, 0)
+		};
+		VarManager.AddVar(var);
+		var.SetPath(new List<Vector2I> { new Vector2I(1, 0), new Vector2I(1, 1), new Vector2I(0, 1) });
+		
+		_var = var;
+	}
+	public override void _Process(double delta)
+	{
+		QueueRedraw();
+	}
+    public override void _Draw()
+    {
+        DrawCircle(_var.Stats.Position, 50, Colors.Red);
+    }
+}

a4238d349100dcf5986b99b2b28060d4d2bcb751
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2026-05-16 01:36:40 +0800

    feat: add basic start menu logic

--- BattleManager/BattleManager.cs
+++ BattleManager/BattleManager.cs
@@ -529,8 +529,10 @@ public partial class BattleManager : Node
 
 	}
 
-	public void InitAlertBorder()
+	public async void InitAlertBorder()
 	{
+		await ToSignal(GetTree().CurrentScene, Node.SignalName.Ready);
+		
 		AlertBorder alertBorder = GetTree().Root.GetNode<AlertBorder>("MainGame/CanvasLayer/AlertBorder");
 		alertBorder.GameData = _gameData;
 	}
